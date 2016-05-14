20e6ce81c8bfc1774aeb7b78834d2d972bef4f3d
Bocha <Bocha>
2016-05-14 10:08:38 -0300

    first push

---
+++ ticketsManager/ticketsManager/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ticketsManager.Startup))]
+namespace ticketsManager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
